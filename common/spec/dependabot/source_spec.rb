thousand-rubies/dependabot-core

commit 49d1a13fca921000b8f9a76612a67d9098431ebd

fix: regex to support Gitlab subgroups

common/lib/dependabot/source.rb
@@ -21,8 +21,8 @@ module Dependabot
     GITLAB_SOURCE = %r{
       (?<provider>gitlab)
       (?:\.com)[/:]
-      (?<repo>[\w.-]+/(?:(?!\.git|\.\s)[\w.-])+)
-      (?:(?:/tree|/blob)/(?<branch>[^/]+)/(?<directory>.*)[\#|/])?
+      (?<repo>(?!\.git|/tree|/blob)[\w./-]+?)(?:\.git)?
+      (?:(?:/tree|/blob)/(?<branch>[^/]+)/(?<directory>.*)[\#|/].*)?$
     }x.freeze
 
     BITBUCKET_SOURCE = %r{

common/spec/dependabot/source_spec.rb
@@ -256,6 +256,13 @@ RSpec.describe Dependabot::Source do
       its(:directory) { is_expected.to eq("dir") }
     end
 
+    context "with a GitLab subgroup URL" do
+      let(:url) { "https://gitlab.com/org/group/abc/blob/master/dir/readme.md" }
+      its(:provider) { is_expected.to eq("gitlab") }
+      its(:repo) { is_expected.to eq("org/group/abc") }
+      its(:directory) { is_expected.to eq("dir") }
+    end
+
     context "with an Azure DevOps URL" do
       let(:url) { "https://dev.azure.com/greysteil/_git/dependabot-test?path" }
       its(:provider) { is_expected.to eq("azure") }

commit 9f04fd57e5ceeac53c9bb140106e6166465aeb93

Fix error when parsing Gitlab changelogs
This bug was causing Dependabot to not be able to create PRs to bump the
oauth2 ruby gem.

This is a fairly popular Ruby gem. It's hosted a gitlab.com, and uses
gemspec metadata to declare how to find it's changelog.

The URL it uses is
`https://gitlab.com/oauth-xx/oauth2/-/tree/v2.0.9/CHANGELOG.md`, which
Dependabot was not parsing properly, causing runtime errors.

This is a regression from 49d1a13fca921000b8f9a76612a67d9098431ebd.

Before, when trying to create a PR bump the oauth2 rubygem, you would
get:

```
🌍 https://rubygems.org/api/v1/gems/oauth2.json
🌍 https://gitlab.com/oauth-xx/oauth2/-/tree/v2.0.9.git/info/refs
/home/dependabot/dependabot-core/common/lib/dependabot/git_metadata_fetcher.rb:68:in `fetch_upload_pack_for': Server error at https://gitlab.com/oauth-xx/oauth2/-/tree/v2.0.9/: Internal server error (RuntimeError)
	from /home/dependabot/dependabot-core/common/lib/dependabot/git_metadata_fetcher.rb:17:in `upload_pack'
	from /home/dependabot/dependabot-core/common/lib/dependabot/git_metadata_fetcher.rb:23:in `tags'
	from /home/dependabot/dependabot-core/common/lib/dependabot/metadata_finders/base/commits_finder.rb:177:in `fetch_dependency_tags'
	from /home/dependabot/dependabot-core/common/lib/dependabot/metadata_finders/base/commits_finder.rb:169:in `dependency_tags'
	from /home/dependabot/dependabot-core/common/lib/dependabot/metadata_finders/base/commits_finder.rb:60:in `new_tag'
	from /home/dependabot/dependabot-core/common/lib/dependabot/metadata_finders/base/changelog_finder.rb:151:in `tag_for_new_version'
	from /home/dependabot/dependabot-core/common/lib/dependabot/metadata_finders/base/changelog_finder.rb:106:in `relevant_tag_changelog'
	from /home/dependabot/dependabot-core/common/lib/dependabot/metadata_finders/base/changelog_finder.rb:70:in `changelog'
	from /home/dependabot/dependabot-core/common/lib/dependabot/metadata_finders/base/changelog_finder.rb:33:in `changelog_url'

common/lib/dependabot/source.rb
@@ -21,8 +21,8 @@ module Dependabot
     GITLAB_SOURCE = %r{
       (?<provider>gitlab)
       (?:\.com)[/:]
-      (?<repo>(?!\.git|/tree|/blob)[\w./-]+?)(?:\.git)?
-      (?:(?:/tree|/blob)/(?<branch>[^/]+)/(?<directory>.*)[\#|/].*)?$
+      (?<repo>[^/]+/(?:(?!\.git)[^/])+((?!/tree|/blob/|/-)/[^/]+)?)
+      (?:(?:/tree|/blob)/(?<branch>[^/]+)/(?<directory>.*)[\#|/].*)?
     }x.freeze
 
     BITBUCKET_SOURCE = %r{

common/spec/dependabot/source_spec.rb
@@ -256,6 +256,13 @@ RSpec.describe Dependabot::Source do
       its(:directory) { is_expected.to eq("dir") }
     end
 
+    context "with a GitLab changelog link" do
+      let(:url) { "https://gitlab.com/oauth-xx/oauth2/-/tree/v2.0.9/CHANGELOG.md" }
+      its(:provider) { is_expected.to eq("gitlab") }
+      its(:repo) { is_expected.to eq("oauth-xx/oauth2") }
+      its(:directory) { is_expected.to be_nil }
+    end
+
     context "with a GitLab subgroup URL" do
       let(:url) { "https://gitlab.com/org/group/abc/blob/master/dir/readme.md" }
       its(:provider) { is_expected.to eq("gitlab") }